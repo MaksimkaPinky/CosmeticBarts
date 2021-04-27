2c29a79ed78fe4a8578b9b195fc0ff33b8c3fd85
315 <315@315NEW-10>
2021-04-27 10:29:49 +0300

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,40 @@
+namespace CosmeticBarts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Product()
+        {
+            ProductSale = new HashSet<ProductSale>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Cost { get; set; }
+
+        public string Description { get; set; }
+
+        public string MainImagePath { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int? ManufacturerID { get; set; }
+
+        public virtual Manufacturer Manufacturer { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductSale> ProductSale { get; set; }
+    }
+}
